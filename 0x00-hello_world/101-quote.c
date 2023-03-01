7846b3a278e855ed80ecf3ad51474ba1a4f4978f
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-01 13:00:16 -0800

    unistd write func

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - prints the passed string
  * Return: 1 if the code sucessed, else return other number
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
