68474e9207f5f195799800ce0ec8dbfe6f0b2c3e
Yoseph Alemu <lejyoseph@gmail.com>
2023-02-28 19:49:05 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/*
+ * print using puts function
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
