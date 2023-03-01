11bb594388059d2dfccbcdbdc342137905d71325
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-01 12:48:34 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the passed string
+ * Return: 1 if the code sucessed, else return other number
+ */
+
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
