eefc49dc0b451b4d09e3e3d18441269796e029ec
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-02 12:45:19 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets
+ * Return: always 0
+ */
+
+int main(void)
+{
+	for (char character = "a"; character <= "z"; character++)
+	{
+		putchar(character);
+	}
+
+	return (0);
+}
