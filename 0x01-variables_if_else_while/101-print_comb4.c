dd5e5b5e62f0415d4a9fc8f6649e93e8c95e66d6
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-04 06:27:19 -0800

    three digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print three digit numbers
+ * Return: 0 if the code successed
+ */
+
+int main(void)
+{
+        int a;
+
+        int b;
+
+        int c;
+        
+        for (a = 48; a <= 55; a++)
+        {
+                for (b = a + 1; b <= 56; b++)
+                {
+                    for (c = b + 1; c <= 57; c++)
+                    {
+                        putchar(a);
+                        putchar(b);
+                        putchar(c);
+                        if (a != 55 || b != 56 || c != 57)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                    }
+                }
+        }
+        return (0);
+}
