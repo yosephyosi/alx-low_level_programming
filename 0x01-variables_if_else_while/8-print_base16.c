c642b6420a52c0c974122afa86bfa0ac191bfbf7
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-03 18:57:57 -0800

    hexadecimal chars

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print hexadecimal charachters
+ * Return: 0 is the code sucessed
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	for (c = 61; c <= 66; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n);
+	return (0);
+}
