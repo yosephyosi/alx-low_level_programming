3fa4e8febe4a33fd060f249811fa6b6b93c97840
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-04 06:32:23 -0800

    three digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,28 +7,29 @@
 
 int main(void)
 {
-        int a;
+	int a;
 
-        int b;
+	int b;
 
-        int c;
-        
-        for (a = 48; a <= 55; a++)
-        {
-                for (b = a + 1; b <= 56; b++)
-                {
-                    for (c = b + 1; c <= 57; c++)
-                    {
-                        putchar(a);
-                        putchar(b);
-                        putchar(c);
-                        if (a != 55 || b != 56 || c != 57)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                    }
-                }
-        }
-        return (0);
+	int c;
+
+	for (a = 48; a <= 55; a++)
+	{
+		for (b = a + 1; b <= 56; b++)
+		{
+			for (c = b + 1; c <= 57; c++)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a != 55 || b != 56 || c != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
