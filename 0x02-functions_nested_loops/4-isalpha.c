f43abed38921fcf0f8b0b1a1c6cff94903e93ff4
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-07 13:05:11 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print 1 if true
+ * @c: recive alphabets
+ * Return: 0
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
