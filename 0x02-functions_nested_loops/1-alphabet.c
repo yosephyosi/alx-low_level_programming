1e9403d8d30a5566464e5064f6825ed3cac79b01
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-07 12:19:15 -0800

    alphabet with putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Main function
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = 97; alphabet <= 122; alphabet++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	return (0);
+}
