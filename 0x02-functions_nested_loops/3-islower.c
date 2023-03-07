975685578be626619dfac586b6e1bfc310bd3980
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-07 12:50:42 -0800

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,16 +2,15 @@
 
 /**
  * _islower - return 1 if its lower
+ * c - recives charachtersii
  * Return: 0
  */
 
 int _islower(int c)
 {
-	if (c < 97 && c > 122)
-	{
-		return (0);
-	}else
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
+	return (0);
 }
