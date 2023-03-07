6cc325e071e3d231e9054fbb0beb969e537df744
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-07 12:29:17 -0800

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	char y;
+	
+	for (x = 0; x <= 9; x++)
+	{
+		for(y = 97;i y <= 122; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
