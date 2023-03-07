7bf7ed368313da6c1f5479f5599b43f520f698eb
Yoseph Alemu <lejyoseph@gmail.com>
2023-03-07 12:07:39 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
+#include "main.h"
 
+/**
+ * main - Main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a[10] = "_putchar";
+	
+	int b;
+
+	for (b = 0; b <= 8; b++)
+	{
+		_putchar(a[b]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
