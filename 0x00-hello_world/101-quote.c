ba1d8004888b377d4de3f4cb91db730388034838
Yoseph <lejyoseph@gmail.com>
2023-05-22 23:56:27 +0300

    other way to write data

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, quote, 59);
+	write(1, q, 59);
 	return (1);
 }
