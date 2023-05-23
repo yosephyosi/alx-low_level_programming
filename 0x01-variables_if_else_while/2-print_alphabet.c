0a1f478908edb7dd75e536344d1f90eaa60d4f4e
Yoseph <lejyoseph@gmail.com>
2023-05-24 00:12:48 +0300

    linter err solved

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-    char alpha = 'a';
+	char alpha = 'a';
 
-    while (alpha <= 'z')
-    {
-        putchar(alpha);
-        ++alpha;
-    }
-    putchar('\n')
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		++alpha;
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
