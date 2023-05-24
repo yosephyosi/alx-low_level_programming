50aa3faf065173d32af81901923cd8b7221a9b2e
Yoseph <lejyoseph@gmail.com>
2023-05-24 20:22:07 +0300

    all decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int num;
+	
+	num = 0;
+	while (num > 10)
+	{
+		printf("%d", num);
+		++num;
+	}	
+	return (0);
+}
