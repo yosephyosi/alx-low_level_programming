a721924bb104f871f8f50879957ec94f268508cd
Yoseph <lejyoseph@gmail.com>
2023-05-25 19:26:35 +0300

    fixed indentation problem

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,26 +8,26 @@
 
 int main(void)
 {
-        int a = 48;
-        int b = 49;
+	int a = 48;
+	int b = 49;
 
-        while (a < 57)
-        {
-                while (b <= 57)
-                {
-                        putchar(a);
-                        putchar(b);
-                        if ((a != 56) || (b != 57))
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                        b++;
-                }
-                a++;
-                b = a +1;
-        }
+	while (a < 57)
+	{
+		while (b <= 57)
+		{
+			putchar(a);
+			putchar(b);
+			if ((a != 56) || (b != 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		b = a +1;
+	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
 
