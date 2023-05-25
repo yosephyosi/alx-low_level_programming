9182a13bfef85bf9c613be6ffeb0b0db53b36e13
Yoseph <lejyoseph@gmail.com>
2023-05-25 19:38:24 +0300

    hexcode

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 
 /**
-* main - Print the numbers of base 16 in lowercase
-*
-* Return: Always 0 (Success)
+ * main - Print the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-		char digit, letter;
+	char digit, letter;
 
-		digit = '0';
-		while (digit <= '9')
-			putchar(digit);
-			++digit;
+	digit = '0';
+	letter = 'a';
+	while (digit <= '9')
+	{
+		putchar(digit);
+		++digit;
+	}
 
-		letter = 'a';
-		while (letter <= 'f')
-			putchar(letter);
-
+	while (letter <= 'f')
+	{
+		putchar(letter);
 		putchar('\n');
-
+	}
 	return (0);
 }
