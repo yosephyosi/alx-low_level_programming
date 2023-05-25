1b9d9c2f3121d97c5bc211ab2f2b952a097a11dc
Yoseph <lejyoseph@gmail.com>
2023-05-26 00:18:00 +0300

    alphaebts in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+*/
+
+void print_alphabet(void)
+{
+        char ch = 'a';
+	while (1 <= 10)
+	{
+        	while (ch <= 'z')
+        	{
+                	_putchar(ch);
+                	ch++;
+        	}
+        	_putchar('\n');
+	}
+	_putchar('\n');
+}
+~        
