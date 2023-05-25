6a8c16f3ad968c063505b56fc90278f59f1bdb99
Yoseph <lejyoseph@gmail.com>
2023-05-26 00:13:52 +0300

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+*/
+
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	return (0);
+}
