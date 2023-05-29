26d9ffc40c9ea8470e059a1277b19674b8dfb79c
Yoseph Alemu <lejyoseph@gmail.com>
2023-05-29 13:36:10 +0300

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: check cahr
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+}
