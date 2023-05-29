7915fa00e7ab2f7885e8ca319e2d919279ba4ea1
Yoseph Alemu <lejyoseph@gmail.com>
2023-05-29 13:42:13 +0300

    is digit method

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/***
+ * _isdigit - Entry point
+ * @c: char checker
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48&& c <= 57)
+	{
+		return (1);
+	
+	}
+
+	return (0);
+}
+
