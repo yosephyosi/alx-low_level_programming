125da6f39f833882168a4b0a2b505cd18392bee5
Yoseph <lejyoseph@gmail.com>
2023-05-29 22:26:08 +0300

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,17 +6,17 @@
 
 void print_numbers(void)
 {
-        int num = 48;
-        int tester = 1;
+	int num = 48;
+	int tester = 1;
 
-        while (tester)
-        {
-                _putchar(num);
-                _putchar('\n');
-                if (num == 57)
-                {
-                        tester = 0;
-                }
-                num++;
-        }
+	while (tester)
+	{
+		_putchar(num);
+		if (num == 57)
+		{
+			tester = 0;
+		}
+		num++;
+	}
+	_putchar('\n');
 }
