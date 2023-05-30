4ccdf2ca12ee3387f3e693a4e66787c6859f3a1f
Yoseph <lejyoseph@gmail.com>
2023-05-31 01:38:03 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,16 @@ int main(void)
 
 	while (num <= 100)
 	{
-		if ((num % 3) == 0 && (num % 5) == 0)
+		if (num % 3 == 0 && num % 5 == 0)
 		{
 			printf("FizzBuzz");
-		}else if ((num % 3) == 0)
+		} else if (num % 3 == 0)
 		{
 			printf("Fizz");
-		}else if ((num % 5) == 0)
+		} else if (num % 5 == 0)
 		{
 			printf("Buzz");
-		}else
+		} else
 		{
 			printf("%d", num);
 		}
