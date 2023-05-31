3a2f569733f431778f8e6f05698f2d0e7ea5dc18
Yoseph <lejyoseph@gmail.com>
2023-06-01 01:27:34 +0300

    str rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev - entry point
+ *@s: string input
+*/
+
+void print_rev(char *s)
+{
+	int length;
+	int counter = 0;	
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	length = counter - 1;
+	while (length >= 0)
+	{
+		length = counter - 1;
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
