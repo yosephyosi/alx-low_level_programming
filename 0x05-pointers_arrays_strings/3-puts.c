14c84c121642dd14a2e2d1029b025a007fcb6b8d
Yoseph <lejyoseph@gmail.com>
2023-06-01 00:10:16 +0300

    puts method

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_puts - entry point
+ *@str: string input
+ */
+
+void _puts(char *str)
+{
+	int counter = 0;
+
+	while (str[counter] != '\n')
+	{
+		_putchar(s[counter]);
+		counter++
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 void reset_to_98(int *n);
 #endif
