2035d0439dfa7dd3efdc7467f386cdafeddbea3d
Yoseph <lejyoseph@gmail.com>
2023-06-01 01:35:27 +0300

    main header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,9 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 void rev_string(char *s);
 void reset_to_98(int *n);
 #endif
