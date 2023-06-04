b45f3aaf7c9826e8da6ecfaae56aa0ddecd14724
Yoseph <lejyoseph@gmail.com>
2023-06-05 00:03:15 +0300

    custom strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,20 +9,24 @@
 int _strcmp(char *s1, char *s2)
 {
 	int index  = 0;
+	int tempIndex1 = 0;
+	int tempIndex2 = 0;
 	int s1Len = 0;
 	int s2Len = 0;
 	int a = 0;
 	char *b = "";
 	int value = 0;
 
-	while (s1 != '\0')
+	while (s1[tempIndex1] != '\0')
 	{
 		s1Len++;
+		tempIndex1++;
 	}
 
-	while (s1 != '\0')
+	while (s2[tempIndex2] != '\0')
         {
                 s2Len++;
+		tempIndex2++;
         }
 
 	if (s1Len > s2Len)
