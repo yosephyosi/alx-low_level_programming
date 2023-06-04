b3ea05b72e3f567ee0deea175502551a0849f0d4
Yoseph <lejyoseph@gmail.com>
2023-06-05 00:23:41 +0300

    custom strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -39,20 +39,20 @@ int _strcmp(char *s1, char *s2)
 		b = s2;
 	}
 
-	while (b[index] != '\0' && index <= (a - 1))
+	while (b[inde] != '\0' && inde <= (a - 1))
 	{
-		if (s1[index] > s2[index])
+		if (s1[inde] > s2[inde])
 		{
-			value = s1[index] - s2[index];
+			value = s1[inde] - s2[inde];
 
 			return (value);
-		} else if (s1[index] < s2[index])
+		} else if (s1[inde] < s2[inde])
 		{
-			value = s1[index] - s2[index];
+			value = s1[inde] - s2[inde];
 
 			return (value);
 		}
-		index++;
+		inde++;
 	}
 	return (0);
 }
