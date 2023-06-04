3572640a8660f515c92917492c6b13a3f2868190
Yoseph <lejyoseph@gmail.com>
2023-06-04 09:06:33 +0300

    custom strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncpy - entry point
+ * @dest: destination of charachter
+ * @src: source of charachter
+ * @n: char length
+ * Return: destination string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n && dest[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	while (a < n)
+	{
+		dest[a] = '\n';
+		a++;
+	}
+	return (dest);
+}		
