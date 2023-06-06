6913990c19eb0b1f9f3f83d333ebbdbd3a89d41d
Yoseph <lejyoseph@gmail.com>
2023-06-06 20:51:27 +0300

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+*/
+
+char *cap_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	char lists[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a] != '\0')
+	{
+		while (s[a] == list[b])
+		{
+			s[a] = s[a] - 32;
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
