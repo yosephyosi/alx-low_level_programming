a95a6ed7f2ffba46c2ecf5a57994a624dac6aa4d
Yoseph <lejyoseph@gmail.com>
2023-06-06 06:01:55 +0300

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_arry - reverse array of int
+ * @a: list of numbers to be reversed
+ * @n: size of list/arrys (length)
+*/
+
+void reverse_arry(int *a, int n)
+{
+	int counter = 0;
+	int bigNum = n - 1;
+	int temp;
+
+	while (counter < n/2)
+	{
+		temp = a[bigNum];
+		a[bigNum] = a[counter];
+		a[counter] = temp;
+		counter++;
+	}
+}
