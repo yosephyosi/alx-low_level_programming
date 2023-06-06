b48b195d4f8d7b1def7fb63f7c00e86a81a21e09
Yoseph <lejyoseph@gmail.com>
2023-06-06 20:07:05 +0300

    to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,15 +6,13 @@
 */
 
 char *string_toupper(char *s)
-{	
+{
 	int counter = 0;
 
 	while (s[counter] != '\0')
 	{
 		if (s[counter] <= 'z' && s[counter] >= 'a')
-		{
 			s[counter] = s[counter] - 32;
-		}
 		counter++;
 	}
 	return (s);
