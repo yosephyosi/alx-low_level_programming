200d8239593e26cff022a2eeeff4800a23732d93
Yoseph <lejyoseph@gmail.com>
2023-06-08 06:19:14 +0300

    leet string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - string to leet 
+ * @s: input for string
+ * Return: leet string
+*/
+
+char *leet(char *s)
+{
+	int a = 0;
+	int b = 0;
+	
+	char *s1 = "aAeEoOtTlL";
+	char *s2 = "4433007711";
+
+	while (s[a] != '\0')
+	{
+		while (b < 10)
+		{
+			if (s[a] == s[b])
+			{
+				s[a] = s2[b];
+			}
+			b++;
+		}
+		a++;
+		b = 0;
+	}
+	return (s);
+}
