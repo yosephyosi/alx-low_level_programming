cb03d852004d56c5b5ef42d1f12c780e783f9b4a
Yoseph <lejyoseph@gmail.com>
2023-06-08 21:36:49 +0300

    main header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
 char *leet(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void reverse_array(int *a, int n);
 char *_strncat(char *dest, char *src, int n);
 #endif
