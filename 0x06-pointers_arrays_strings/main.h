7c1a93a9d2a7619f5a82dac9830ce992450937d3
Yoseph <lejyoseph@gmail.com>
2023-06-08 21:16:36 +0300

    print number with putchar

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ char *string_toupper(char *);
 int _strcmp(char *s1, char *s2);
 char *cap_string(char *);
 char *rot13(char *);
+void print_number(int n);
 char *leet(char *);
 void reverse_array(int *a, int n);
 char *_strncat(char *dest, char *src, int n);
